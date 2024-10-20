623318509574da7d22a45dc259a1eba1c8280758
crispy-caesus <114518720+crispy-caesus@users.noreply.github.com>
2024-10-20 13:13:18 +0200

    delete test files

--- test.py
+++
@@ -1,8 +0,0 @@
-import db as database
-import asyncio
-
-async def main():
-    db = database.Database("1170646611236487208.db")
-    await db.select_club_by_owner(621759962280099840)
-
-asyncio.run(main())
